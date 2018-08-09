406a5310802481d856dc15aa9c0db9b9cb1f024f
Dmitry Tselinko <dmitry.tselinko@gmail.com>
2018-08-10 00:10:03 +0300

    move to boilerplate solution

--- src/ui/components/pages/index.js
+++
@@ -1,5 +0,0 @@
-export { default as AuthPage } from './AuthPage';
-export { default as TablesListPage } from './TablesListPage';
-export { default as TablePage } from './TablePage';
-
-export default {};
--- src/ui/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/molecules/App';
-import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
